20e3078003f4831619493eac0e299744b0ee15f0
Anxietymode <ilsatfarhodovic@gmail.com>
2026-04-22 10:45:47 +0500

    dodelal

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuToggle = document.getElementById('menuToggle');
     const sidebar = document.querySelector('.sidebar');
+    const overlay = document.getElementById('sidebarOverlay');
 
-    if (menuToggle) {
-        menuToggle.addEventListener('click', () => {
-            sidebar.classList.toggle('open');
-            menuToggle.classList.toggle('active');
-            
-           
-            if (sidebar.classList.contains('open')) {
-                document.body.style.overflow = 'hidden';
-            } else {
-                document.body.style.overflow = '';
-            }
-        });
+    
+    function toggleMenu() {
+        sidebar.classList.toggle('open');
+        menuToggle.classList.toggle('active');
+        overlay.classList.toggle('active');
+        
+        
+        if (sidebar.classList.contains('open')) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
     }
 
+    if (menuToggle) {
+        menuToggle.addEventListener('click', toggleMenu);
+    }
+
+  
+    if (overlay) {
+        overlay.addEventListener('click', toggleMenu);
+    }
+
+    
     const items = document.querySelectorAll('.menu-item');
     items.forEach(item => {
         item.addEventListener('click', () => {
             if (window.innerWidth <= 800) {
-                sidebar.classList.remove('open');
-                menuToggle.classList.remove('active');
-                document.body.style.overflow = '';
+                toggleMenu();
             }
         });
     });
